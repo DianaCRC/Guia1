d0ae8bb4ed42cfefb6d774981c94fa6c1a79195b
DianaCRC <drodrig08863@universidadean.edu.co>
2024-08-17 22:56:48 -0500

    Add files via upload

---
+++ Actividad 1/estudiante.java
@@ -0,0 +1,40 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class estudiante extends persona {
+
+    /**
+     * Default constructor
+     */
+    public estudiante() {
+    }
+
+    /**
+     * 
+     */
+    public void colegio_grado;
+
+    /**
+     * 
+     */
+    public void fecha_ingreso;
+
+    /**
+     * 
+     */
+    public void cursos_inscritos() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void inscribir_curso() {
+        // TODO implement here
+    }
+
+}
---
+++ Actividad 1/persona.java
@@ -0,0 +1,67 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class persona {
+
+    /**
+     * Default constructor
+     */
+    public persona() {
+    }
+
+    /**
+     * 
+     */
+    public void nombre_persona;
+
+    /**
+     * 
+     */
+    public void cedula;
+
+    /**
+     * 
+     */
+    public void fecha_nacimiento;
+
+    /**
+     * 
+     */
+    public void ciudad_nacimiento;
+
+    /**
+     * 
+     */
+    public void departamento_nacimiento;
+
+    /**
+     * 
+     */
+    public void pais_nacimiento;
+
+    /**
+     * 
+     */
+    public void guardar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void modificar() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void calcular_edad() {
+        // TODO implement here
+    }
+
+}
